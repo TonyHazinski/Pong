1d548f9514e41587faed30e5f184184bd035839b
Tony Hazinski <hazaj2000@gmail.com>
2022-07-28 20:01:08 -0500

    major changes

--- pong.js
+++ pong.js
@@ -8,7 +8,7 @@ class Ball {
         this.y = 277;
         this.xVelocity = 1;
         this.yVelocity = 1;
-        this.radius = 250;
+        this.radius = 15;
     }
 
     draw() {
@@ -23,24 +23,24 @@ class Ball {
         this.x += this.xVelocity;
         this.y += this.yVelocity;
         if (this.x <= (0 + this.radius)) {
-            this.xVelocity = 1;
+            this.xVelocity *= -1;
         }
         if (this.x + this.radius >= 600) {
-            this.xVelocity = -1;
+            this.xVelocity *= -1;
         }
         if (this.y <= (0 + this.radius)) {
-            this.yVelocity = 1;
+            this.yVelocity *= -1;
         }
         if (this.y + this.radius >= 600) {
             gameOver();
-            this.yVelocity = -1;
+            this.yVelocity *= -1;
         }
     }
 }
 
 class Paddle {
     constructor() {
-        this.width = 600;
+        this.width = 100;
         this.height = 20;
         this.x = 300 - this.width/2;
         this.y = 600 - this.height;
@@ -48,14 +48,20 @@ class Paddle {
     }
     
     draw() {
-        this.x += this.xVelocity;
         ctx.beginPath();
         ctx.rect(this.x, this.y, this.width, this.height)
         ctx.fillStyle = 'yellow';
         ctx.fill();
         ctx.stroke();
     }
-    move(key) {
+
+    move() {
+        if (this.x <= 0  && this.xVelocity == -1 || this.x + this.width >= 600 && this.xVelocity == 1) {
+            return;
+        }
+        this.x += this.xVelocity;
+    }
+    changeDirection(key) {
         if (key == "ArrowRight") {
             this.xVelocity = 1;
         }
@@ -76,15 +82,17 @@ class Paddle {
 
 let ball = new Ball();
 let paddle = new Paddle();
-window.addEventListener('keydown', (event) => paddle.move(event.key));
+window.addEventListener('keydown', (event) => paddle.changeDirection(event.key));
 window.addEventListener('keyup', (event) => paddle.stopMoving(event.key))
 setInterval(() => {
     ctx.clearRect(0, 0, 600, 600);
     ball.move();
     ball.draw();
+    paddle.move();
     paddle.draw();
     if (ball.y + ball.radius == paddle.y && ball.x >= paddle.x && ball.x <= paddle.x + paddle.width) {
         ball.yVelocity = -1;
+        ball.xVelocity = ((Math.random() * 4) -2)
     }
     if (ball.y + ball.radius > paddle.y && ball.x + ball.radius > paddle.x && ball.x < paddle.x + paddle.width/2) {
         ball.yVelocity = -1;
