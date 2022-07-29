fda13cadf78bc6d8b9a898dcabf4388008424bbe
Tony Hazinski <hazaj2000@gmail.com>
2022-07-28 23:34:24 -0500

    ok

--- pong.js
+++ pong.js
@@ -2,17 +2,28 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d")
 const canvasWidth = 600;
 const canvasHeight = 600;
+let running = false;
 
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min
 }
 
+function gameOver() {
+    running = false;
+    ball.x = canvasWidth/2;
+    ball.y = canvasHeight/2;
+    ball.xVelocity = 0;
+    ball.yVelocity = 0;
+    paddle.x = canvasWidth/2 - paddle.width/2
+    paddle.xVelocity = 0;
+}
+
 class Ball {
     constructor() {
-        this.x = 27;
-        this.y = 277;
-        this.xVelocity = 1;
-        this.yVelocity = 1;
+        this.x = canvasWidth/2;
+        this.y = canvasHeight/2;
+        this.xVelocity = 0;
+        this.yVelocity = 0;
         this.radius = 15;
     }
 
@@ -87,6 +98,14 @@ class Paddle {
 
 let ball = new Ball();
 let paddle = new Paddle();
+window.addEventListener('keydown', (event) => {
+    console.log(event.key)
+    if (event.key == " " && !running) {
+        ball.xVelocity = 1;
+        ball.yVelocity = 1;
+        running = true;
+    }
+})
 window.addEventListener('keydown', (event) => paddle.changeDirection(event.key));
 window.addEventListener('keyup', (event) => paddle.stopMoving(event.key))
 setInterval(() => {
